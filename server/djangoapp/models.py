17f9f4c8ffa823ba0cba21d50ea30c0d73122654
ritanene <rita.anene@firstally.com>
2024-05-19 17:51:04 -0400

    added cars and car model

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,7 +12,12 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +28,41 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    CONVERTIBLE = 'Convertible'
+    HATCHBACK = 'Hatchback'
+    TRUCK = 'Truck'
+    VAN = 'Van'
+    OTHER = 'Other'
+
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (CONVERTIBLE, 'Convertible'),
+        (HATCHBACK, 'Hatchback'),
+        (TRUCK, 'Truck'),
+        (VAN, 'Van'),
+        (OTHER, 'Other'),
+    ]
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN,
+    )
+    year = models.IntegerField(default=2023,
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+    )
+    color = models.CharField(max_length=50, blank=True, null=True)  # Optional field for car color
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Optional field for car price
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
