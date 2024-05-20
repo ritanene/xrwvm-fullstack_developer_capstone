eb4ce7709edc464a26e9ce7c747b8b843cadb6c4
ritanene <rita.anene@firstally.com>
2024-05-19 20:13:06 -0400

    added dealers page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -86,6 +86,14 @@ def registration(request):
 # a list of dealerships
 # def get_dealerships(request):
 # ...
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
